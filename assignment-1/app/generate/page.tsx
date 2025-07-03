b6e66e8b92104ce6983a8a350b62b89230a31cef
Saad Tahir <tahirsaad99@gmail.com>
2025-07-03 14:21:38 +0500

    refactor: remove generate page

--- assignment-1/app/generate/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function GeneratePage() {
-    return (
-        <main className="p-8">
-            <h1 className="text-2xl font-bold mb-4">Quote Generator</h1>
-            <p>Form will go here soon!</p>
-        </main>
-    );
-}
